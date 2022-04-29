0477f3bbe548fd526d9ad537d6314c9d8ff131ee
Rohan Sikder <80963667+rohansikder@users.noreply.github.com>
2022-04-29 02:03:06 +0100

    Delete Main_Site_Folder directory

--- Main_Site_Folder/About/About.js
+++
@@ -1,6 +0,0 @@
-/*START OF SIDE BAR: Reference: https://codepen.io/andgatjens/pen/XWJPddE (RS)*/
-$("#menu-toggle").click(function(e) {
-    e.preventDefault();
-    $("#wrapper").toggleClass("toggled");
-  });
-/*END OF SIDE BAR: Reference: https://codepen.io/andgatjens/pen/XWJPddE (RS)*/
